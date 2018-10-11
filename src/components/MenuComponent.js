61719ccb5c65537425aa210994add9ed05706e7c
Vijay Adhithyan Mohan <vijay@thewebco.co.nz>
2018-10-11 22:55:15 +1300

    Component Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,72 +1,52 @@
 import React, {Component} from 'react';
-import {Media} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardTitle, CardText, CardBody} from 'reactstrap';
 class Menu extends Component{
     constructor(props){
         super(props);
         this.state = {
-            dishes: [
-                {
-                    id: 0,
-                    name:'Uthappizza',
-                    image: './uthappizza.png',
-                    category: 'mains',
-                    label:'Hot',
-                    price:'4.99',
-                    description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-                },
-                 {
-                    id: 1,
-                    name:'Zucchipakoda',
-                    image: '../../images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label:'',
-                    price:'1.99',
-                    description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-                },
-                 {
-                    id: 2,
-                    name:'Vadonut',
-                    image: '../../images/vadonut.png',
-                    category: 'appetizer',
-                    label:'New',
-                    price:'1.99',
-                    description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
-                },
-                 {
-                    id: 3,
-                    name:'ElaiCheese Cake',
-                    image: '../../images/elaicheesecake.png',
-                    category: 'dessert',
-                    label:'',
-                    price:'2.99',
-                    description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-                }
-            ],
-        };
-        
+            selectedDish: null
+        }
+    }
+    onSelectedDish(dish){
+        this.setState( {selectedDish : dish} );
+    }
+    renderDish(dish){
+        if(dish != null){
+            return(
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText heading>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            );
+            
+        }else{
+
+        }
     }
     render(){
-        const menu = this.state.dishes.map((dish) => {
+        const menu = this.props.dishes.map((dish) => {
             return(
-                <div key ={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                    <Media left middle>
-                        <Media object src={dish.image} alt={dish.name}></Media>
-                    </Media>
-                    <Media body className="ml-5">
-                        <Media heading={dish.name}></Media>
-                        <p>{dish.description}</p>
-                    </Media>
-                </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card tag="li" onClick = {()=>this.onSelectedDish(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+
+                        <CardImgOverlay className="ml-5">
+                            <CardText heading>{dish.name}</CardText>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         });
         return(
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
